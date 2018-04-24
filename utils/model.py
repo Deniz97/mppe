3e577af164ae0bafd1dfed8d5cbf06ff622c3fb2
mkocabas <muhammed.kocabas@metu.edu.tr>
2018-04-24 10:16:23 +0300

    update fpn

--- utils/model.py
+++ utils/model.py
@@ -8,14 +8,16 @@ from torch.autograd import Variable
 # FPN paper: https://arxiv.org/abs/1612.03144
 # A new idea: https://arxiv.org/pdf/1803.01534.pdf
 
-class FPN(torch.nn.Module):
+class FPN(nn.Module):
     def __init__(self):
         """
         In the constructor we instantiate two nn.Linear modules and assign them as
         member variables.
         """
         super(FPN, self).__init__()
+        self.feature_size = 256
         self.resnet = torchvision.models.resnet50(pretrained=True)
+        self.up = nn.UpsamplingBilinear2d(scale_factor=2)
 
     def forward(self, x):
         x = self.resnet.conv1(x)
@@ -32,13 +34,15 @@ class FPN(torch.nn.Module):
 
         return c2, c3, c4, c5
 
-class Model(torch.nn.Module):
+class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
         self.FPN = FPN()
 
+
     def forward(self, x):
         features = FPN(x)
+        p2, p3, p4, p5 = features
 
         return features
 
